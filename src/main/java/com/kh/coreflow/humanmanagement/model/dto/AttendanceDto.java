12cda3740d8e08e2948dd8b0715d9185a1a8f192
dmddjd <dmstjd99510@naver.com>
2025-09-07 19:08:22 +0900

    2025-09-07 19:08 근퇴관리 구현중

---
+++ src/main/java/com/kh/coreflow/humanmanagement/model/dto/AttendanceDto.java
@@ -0,0 +1,23 @@
+package com.kh.coreflow.humanmanagement.model.dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class AttendanceDto {
+	
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class AttendanceInfo{
+		private String attDate;
+		private String userName;
+		private String depName;
+		private String posName;
+		private String checkInTime;
+		private String checkOutTime;
+		private int status;
+	}
+}
---
+++ src/main/java/com/kh/coreflow/humanmanagement/model/service/AttendanceServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kh.coreflow.humanmanagement.model.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.kh.coreflow.humanmanagement.model.dao.AttendanceDao;
+import com.kh.coreflow.humanmanagement.model.dto.AttendanceDto.AttendanceInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AttendanceServiceImpl implements AttendanceService{
+	private final AttendanceDao dao;
+
+	public List<AttendanceInfo> attInfo(Map<String, Object> params) {
+		return dao.attInfo(params);
+	}
+
+}
