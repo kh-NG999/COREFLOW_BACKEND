a57174c3f02fb10a6f5b7bd2070151881045bdfa
dmddjd <dmstjd99510@naver.com>
2025-09-13 18:21:21 +0900

    2025-09-13 18:21 조직도 완성

--- src/main/java/com/kh/coreflow/humanmanagement/model/dao/OrganizationDao.java
+++ src/main/java/com/kh/coreflow/humanmanagement/model/dao/OrganizationDao.java
@@ -1,5 +1,17 @@
 package com.kh.coreflow.humanmanagement.model.dao;
 
+import java.util.List;
+
+import com.kh.coreflow.humanmanagement.model.dto.MemberDto.MemberResponse;
+import com.kh.coreflow.humanmanagement.model.dto.OrganizationDto.ChildDep;
+import com.kh.coreflow.humanmanagement.model.dto.OrganizationDto.ParentDep;
+
 public interface OrganizationDao {
+
+	List<ParentDep> parentDeptList();
+
+	List<ChildDep> childDeptList(int parentId);
+
+	List<MemberResponse> memberList(int depId);
 
 }
--- src/main/java/com/kh/coreflow/humanmanagement/model/service/OrganizationServiceImpl.java
+++ src/main/java/com/kh/coreflow/humanmanagement/model/service/OrganizationServiceImpl.java
@@ -1,11 +1,33 @@
 package com.kh.coreflow.humanmanagement.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.kh.coreflow.humanmanagement.model.dao.OrganizationDao;
+import com.kh.coreflow.humanmanagement.model.dto.MemberDto.MemberResponse;
+import com.kh.coreflow.humanmanagement.model.dto.OrganizationDto.ChildDep;
+import com.kh.coreflow.humanmanagement.model.dto.OrganizationDto.ParentDep;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OrganizationServiceImpl implements OrganizationService{
+	private final OrganizationDao dao;
 
+	@Override
+	public List<ParentDep> parentDeptList() {
+		return dao.parentDeptList();
+	}
+
+	@Override
+	public List<ChildDep> childDeptList(int parentId) {
+		return dao.childDeptList(parentId);
+	}
+
+	@Override
+	public List<MemberResponse> memberList(int depId) {
+		return dao.memberList(depId);
+	}
 }
